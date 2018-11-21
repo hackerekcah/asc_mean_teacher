b95a5238a023e2cf210114c7c6fd42249c79b21c
hackerekcah <hackerekcah@outlook.com>
2018-11-21 09:04:29 +0800

    add Reporter in history.py

--- utils/history.py
+++ utils/history.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 import numpy as np
+import os
 
 
 class History (object):
@@ -76,3 +77,74 @@ class History (object):
     def clear(self):
         clear_output(wait=True)
 
+
+class Reporter(object):
+    def __init__(self, exp, ckpt_file=None):
+        self.ckpt_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/ckpt'
+        self.exp_path = os.path.join(self.ckpt_root, exp)
+        self.run_list = os.listdir(self.exp_path)
+        self.selected_ckpt = None
+        self.selected_epoch = None
+        self.selected_log = None
+        self.selected_run = None
+
+    def select_best(self, run=""):
+
+        """
+        set self.selected_run, self.selected_ckpt, self.selected_ckpt, self.selected_epoch
+        :param run:
+        :return:
+        """
+
+        matched = []
+        for fname in self.run_list:
+            if fname.startswith(run) and fname.endswith('tar'):
+                matched.append(fname)
+
+        acc = []
+        import re
+        for s in matched:
+            acc_str = re.search('acc_(.*)\.tar', s).group(1)
+            acc.append(float(acc_str))
+
+        best_idx = np.argmax(acc)
+        best_fname = matched[best_idx]
+
+        self.selected_run = best_fname.split(',')[0]
+        self.selected_epoch = int(re.search('Epoch_(.*),acc', best_fname).group(1))
+
+        ckpt_file = os.path.join(self.exp_path, best_fname)
+
+        self.selected_ckpt = ckpt_file
+
+
+        return self
+
+    def pconfig(self, run):
+        if run:
+            self.selected_log = os.path.join(self.exp_path, run+'.log')
+        else:
+            self.selected_log = os.path.join(self.exp_path, self.selected_run + '.log')
+
+        with open(self.selected_log, 'r') as f:
+            config = f.readline()
+            # config = config.split('\n')[0]
+            print("<<<Config:\n{}".format(config))
+
+    def pbest(self, run="", epoch=None):
+        self.select_best(run=run)
+        print("<<<Best: Run:{}, Epoch:{}\n".format(self.selected_run, self.selected_epoch))
+        if run and epoch:
+            print("<<<Select: Run:{}, Epoch:{}\n".format(run, epoch))
+
+        self.pconfig(run=run)
+        import torch
+        histories = torch.load(self.selected_ckpt)['histories']
+        print("<<<Result")
+        for hist in histories:
+            if run and epoch:
+                print("{:10},Epoch{:4d},Acc{:.3f}".format(hist.name, epoch,
+                                                          hist.acc[epoch-1]))
+            else:
+                print("{:10},Epoch{:4d},Acc{:.3f}".format(hist.name, self.selected_epoch,
+                                                          hist.acc[self.selected_epoch - 1]))
